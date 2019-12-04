1224498eb0583653f6422b4dc9db505bc65366d0
MiguelRamirezAgustin <miguel.r.agustin@gmail.com>
2019-12-04 15:11:04 -0600

    veriicacion de sesion en splash

--- app/src/main/java/com/congnituscurso/cognitusproyect/activities/Splash.kt
+++ app/src/main/java/com/congnituscurso/cognitusproyect/activities/Splash.kt
@@ -1,9 +1,11 @@
 package com.congnituscurso.cognitusproyect.activities
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import com.congnituscurso.cognitusproyect.R
 
 class Splash : AppCompatActivity(), Runnable {
@@ -23,8 +25,26 @@ class Splash : AppCompatActivity(), Runnable {
     }
 
     override fun run() {
-     val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+
+        //validar si existe sesion
+        val sharedPreferences= getSharedPreferences("my_aplicacion_binding",Context.MODE_PRIVATE)
+
+        val usrId = sharedPreferences.getString("usr_id", "")
+        Log.d("--->", usrId)
+
+        if (!usrId.equals("")){
+            // si existe sesion
+            val intent = Intent(this, MenuActivity::class.java)
+            intent.putExtra("nombre_usr", usrId)
+            startActivity(intent)
+            finish()
+        }else{
+            //No existe sesion
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
 }
